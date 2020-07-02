f88a9ca6ae1f6fba8d97789e40cf8ecc8e1c4454
Aron S. Talai <sahand.talai@gmail.com>
2020-07-02 15:36:44 -0400

    Update sentiment_analysis.py
    add functions

--- sentiment/sentiment_analysis.py
+++ sentiment/sentiment_analysis.py
@@ -60,3 +60,56 @@ def plot_sentiment_over_time(input_str, num_of_segments):
 	plt.legend(loc = 'upper right');
 	plt.show()
 
+def spacy_binary_text_classifier(input_df, input_label, architecture, epoch_num, batch_size, save_model = 0):
+	''' enable binary text classification pipeline using spacy
+	input1: input dataframe where the format is data_label, content and the column names are label and text
+	input2: input_label where the labels are in a list of strings .i.e. ['ham','spam']
+	input3: spacy architecture to use, options are ensemble, simple_cnn, bow
+	input4: epoch_num: number of epochs to run 
+	input5: batch_size: size of batch during training
+	input6: save the model meta in .py directory, defaults to 0'''
+
+
+	nlp = spacy.blank("en")
+	# Create the TextCategorizer with exclusive classes and "bow" architecture
+	textcat = nlp.create_pipe(
+	              "textcat",
+	              config={
+	                "exclusive_classes": True,
+	                "architecture": "{}".format(architecture)})
+
+	# Add the TextCategorizer to the empty model
+	nlp.add_pipe(textcat)
+
+	# Add labels to text classifier
+	textcat.add_label("{}".format(input_label[0]))
+	textcat.add_label("{}".format(input_label[1]))
+
+
+	train_texts = input_df['text'].values
+	train_labels = [{'cats': {'{}'.format(input_label[0]): label == '{}'.format(input_label[0]),
+	                          '{}'.format(input_label[1]): label == '{}'.format(input_label[1])}} 
+	                for label in input_df['label']]
+
+
+	train_data = list(zip(train_texts, train_labels))
+
+	random.seed(1)
+	spacy.util.fix_random_seed(1)
+	optimizer = nlp.begin_training()
+
+	losses = {}
+	for epoch in range(epoch_num):
+	    random.shuffle(train_data)
+	    # Create the batch generator with batch size = 8
+	    batches = minibatch(train_data, size = batch_size)
+	    # Iterate through minibatches
+	    for batch in batches:
+	        # Each batch is a list of (text, label) but we need to
+	        # send separate lists for texts and labels to update().
+	        # This is a quick way to split a list of tuples into lists
+	        texts, labels = zip(*batch)
+	        nlp.update(texts, labels, sgd=optimizer, losses=losses)
+	    print(losses)
+
+	if save_model: nlp.to_disk("./")
